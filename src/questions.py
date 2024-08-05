b87ec8e1af128fff4a82a102905e2d79364ceac2
Johnson-Mok <55184234+Johnson-Mok@users.noreply.github.com>
2024-08-05 23:42:48 +0200

    create front end

--- src/questions.py
+++ src/questions.py
@@ -1,27 +1,42 @@
-questions = [
+QUESTIONS = [
     {
         "prompt": "What is the capital of France?",
-        "options": ["Berlin", "Paris", "Rome", "Madrid"],
-        "answer": 2,
+        "options": [
+            ("images/Berlin.jpg", "Berlin"),
+            ("images/Paris.jpg", "Paris"),
+            ("images/Rome.jpg", "Rome"),
+            ("images/Madrid.jpg", "Madrid"),
+        ],
+        "answer": "Paris",
     },
     {
         "prompt": "Which planet is known as the Red Planet?",
-        "options": ["Earth", "Mars", "Jupiter", "Saturn"],
-        "answer": 2,
+        "options": [
+            ("images/Earth.jpg", "Earth"),
+            ("images/Mars.jpg", "Mars"),
+            ("images/Jupiter.jpg", "Jupiter"),
+            ("images/Saturn.jpg", "Saturn"),
+        ],
+        "answer": "Mars",
     },
     {
         "prompt": "Who wrote 'To Kill a Mockingbird'?",
         "options": [
-            "Harper Lee",
-            "Mark Twain",
-            "Ernest Hemingway",
-            "F. Scott Fitzgerald",
+            ("images/Earth.jpg", "Harper Lee"),
+            ("images/Earth.jpg", "Mark Twain"),
+            ("images/Earth.jpg", "Ernest Hemingway"),
+            ("images/Earth.jpg", "F. Scott Fitzgerald"),
         ],
-        "answer": 1,
+        "answer": "Harper Lee",
     },
     {
         "prompt": "What is the largest mammal?",
-        "options": ["Elephant", "Blue Whale", "Giraffe", "Rhino"],
-        "answer": 2,
+        "options": [
+            ("images/Earth.jpg", "Elephant"),
+            ("images/Earth.jpg", "Blue Whale"),
+            ("images/Earth.jpg", "Giraffe"),
+            ("images/Earth.jpg", "Rhino"),
+        ],
+        "answer": "Blue Whale",
     },
 ]
